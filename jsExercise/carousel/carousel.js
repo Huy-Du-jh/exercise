8c4dbd5a6f2a60e8e817a92db6fd7c1b5b1ac678
Huy-Du-jh <huy.du@journeyh.io>
2024-03-13 10:56:53 +0700

    wrap all inside a function to not expose functions or variables to global

--- jsExercise/carousel/carousel.js
+++ jsExercise/carousel/carousel.js
@@ -1,52 +1,55 @@
-window.addEventListener("load", e => {
-  const images = document.getElementsByClassName("carouselImage");
-  const imagesContainer = images[0].parentNode;
-  const containerWidthWithUnit = getComputedStyle(imagesContainer).getPropertyValue('width');
-  const containerWidth = Number(containerWidthWithUnit.slice(0, -2));
-  for (i = 0; i < images.length; i++) {
-    images[i].style.left = ((containerWidth) * i) + "px";
-  }
-});
-let currentIndex = 0;
-function swipeLeft(e) {
-  e.stopPropagation();
-  const images = document.getElementsByClassName("carouselImage");
-  const imagesContainer = images[0].parentNode;
-  const containerWidthWithUnit = getComputedStyle(imagesContainer).getPropertyValue('width');
-  const containerWidth = Number(containerWidthWithUnit.slice(0, -2));
-  if (currentIndex === 0) {
+function createCarousel() {
+  window.addEventListener("load", e => {
+    const images = document.getElementsByClassName("carouselImage");
+    const imagesContainer = images[0].parentNode;
+    const containerWidthWithUnit = getComputedStyle(imagesContainer).getPropertyValue('width');
+    const containerWidth = Number(containerWidthWithUnit.slice(0, -2));
     for (i = 0; i < images.length; i++) {
-      images[i].style.transform = `translateX(${-containerWidth * (images.length - 1)}px)`;
+      images[i].style.left = ((containerWidth) * i) + "px";
+    }
+  });
+  let currentIndex = 0;
+  swipeLeft = function (e) {
+    e.stopPropagation();
+    const images = document.getElementsByClassName("carouselImage");
+    const imagesContainer = images[0].parentNode;
+    const containerWidthWithUnit = getComputedStyle(imagesContainer).getPropertyValue('width');
+    const containerWidth = Number(containerWidthWithUnit.slice(0, -2));
+    if (currentIndex === 0) {
+      for (i = 0; i < images.length; i++) {
+        images[i].style.transform = `translateX(${-containerWidth * (images.length - 1)}px)`;
+      }
+    }
+    else {
+      for (i = 0; i < images.length; i++) {
+        images[i].style.transform = `translateX(${-containerWidth * (currentIndex - 1)}px)`;
+      }
+    }
+    currentIndex -= 1;
+    if (currentIndex < 0) {
+      currentIndex = images.length - 1;
     }
   }
-  else {
-    for (i = 0; i < images.length; i++) {
-      images[i].style.transform = `translateX(${-containerWidth * (currentIndex - 1)}px)`;
+  swipeRight = function (e) {
+    e.stopPropagation();
+    const images = document.getElementsByClassName("carouselImage");
+    const imagesContainer = images[0].parentNode;
+    const containerWidthWithUnit = getComputedStyle(imagesContainer).getPropertyValue('width');
+    const containerWidth = Number(containerWidthWithUnit.slice(0, -2));
+    if (currentIndex === images.length - 1) {
+      for (i = 0; i < images.length; i++) {
+        images[i].style.transform = `translateX(${0}px)`;
+      }
     }
-  }
-  currentIndex -= 1;
-  if (currentIndex < 0) {
-    currentIndex = images.length - 1;
+    else {
+      for (i = 0; i < images.length; i++) {
+        images[i].style.transform = `translateX(${-containerWidth * (currentIndex + 1)}px)`
+      }
+    }
+    currentIndex += 1;
+    if (currentIndex > images.length - 1) {
+      currentIndex = 0;
+    }
   }
 }
-function swipeRight(e) {
-  e.stopPropagation();
-  const images = document.getElementsByClassName("carouselImage");
-  const imagesContainer = images[0].parentNode;
-  const containerWidthWithUnit = getComputedStyle(imagesContainer).getPropertyValue('width');
-  const containerWidth = Number(containerWidthWithUnit.slice(0, -2));
-  if (currentIndex === images.length - 1) {
-    for (i = 0; i < images.length; i++) {
-      images[i].style.transform = `translateX(${0}px)`;
-    }
-  }
-  else {
-    for (i = 0; i < images.length; i++) {
-      images[i].style.transform = `translateX(${-containerWidth * (currentIndex + 1)}px)`
-    }
-  }
-  currentIndex += 1;
-  if (currentIndex > images.length - 1) {
-    currentIndex = 0;
-  }
-}
+createCarousel();
--- jsExercise/decisionTree/decisionTree.js
+++ jsExercise/decisionTree/decisionTree.js
@@ -1,95 +1,98 @@
-function Question(key, question, answer = null, parent = null, questions = {}) {
-  this.key = key;
-  this.question = question;
-  this.answer = answer;
-  this.parent = parent;
-  this.questions = questions;
-  this.ask = function () {
-    let answers = "";
-    for (i in questions) {
-      answers = answers + this.questions[i].key + ". " + this.questions[i].answer + "     ";
+function decisionTree() {
+  function Question(key, question, answer = null, parent = null, questions = {}) {
+    this.key = key;
+    this.question = question;
+    this.answer = answer;
+    this.parent = parent;
+    this.questions = questions;
+    this.ask = function () {
+      let answers = "";
+      for (i in questions) {
+        answers = answers + this.questions[i].key + ". " + this.questions[i].answer + "     ";
+      }
+      const choice = prompt(this.question + '\n' + answers);
+      return choice;
     }
-    const choice = prompt(this.question + '\n' + answers);
-    return choice;
+    this.addParent = function (parentKey) {
+      this.parent = parentKey
+    }
+    this.addChildQuestion = function (child) {
+      this.questions[child.key] = child;
+      child.addParent(this.key);
+    }
+    this.deleteChildQuestion = function (key) {
+      delete this.questions[key];
+      delete QUESTIONS[key];
+    }
+    this.deleteAllChildQuestion = function () {
+      for (i in this.questions) {
+        this.questions[i].deleteAllChildQuestion();
+        this.deleteChildQuestion(i);
+      }
+    }
   }
-  this.addParent = function (parentKey) {
-    this.parent = parentKey
+  const QUESTIONS = {};
+  const firstQuestion = new Question(1, "Your age?");
+  const secondQuestion = new Question(2, "Your shcool?", "Under 18");
+  const thirdQuestion = new Question(3, "Your job?", "over 18");
+  const tempQuestion = new Question(4, "Ok", "cambridge")
+  firstQuestion.addChildQuestion(secondQuestion);
+  firstQuestion.addChildQuestion(thirdQuestion);
+  secondQuestion.addChildQuestion(tempQuestion);
+  QUESTIONS[firstQuestion.key] = firstQuestion;
+  QUESTIONS[secondQuestion.key] = secondQuestion;
+  QUESTIONS[thirdQuestion.key] = thirdQuestion;
+  QUESTIONS[tempQuestion.key] = tempQuestion;
+
+  addQuestion = function (e) {
+    e.stopPropagation();
+    const key = Number(document.getElementById("key").value);
+    const question = document.getElementById("question").value;
+    const parent = Number(document.getElementById("parent").value);
+    const answer = document.getElementById("answer").value;
+    const newQuestion = new Question(key, question, answer, parent);
+    QUESTIONS[parent].addChildQuestion(newQuestion);
+    QUESTIONS[key] = newQuestion;
+    console.log(QUESTIONS);
   }
-  this.addChildQuestion = function (child) {
-    this.questions[child.key] = child;
-    child.addParent(this.key);
-  }
-  this.deleteChildQuestion = function (key) {
-    delete this.questions[key];
+
+  deleteQuestion = function (e) {
+    e.stopPropagation();
+    const key = Number(document.getElementById("key").value);
+    QUESTIONS[key].deleteAllChildQuestion();
+    QUESTIONS[QUESTIONS[key].parent].deleteChildQuestion(key);
     delete QUESTIONS[key];
+    console.log(QUESTIONS);
   }
-  this.deleteAllChildQuestion = function () {
-    for (i in this.questions) {
-      this.questions[i].deleteAllChildQuestion();
-      this.deleteChildQuestion(i);
+
+  editQuestion = function (e) {
+    e.stopPropagation();
+    const key = Number(document.getElementById("key").value);
+    const question = document.getElementById("question").value;
+    const parent = Number(document.getElementById("parent").value);
+    const answer = document.getElementById("answer").value;
+    const newQuestion = new Question(key, question, answer, parent);
+    const parentKey = QUESTIONS[key].parent;
+    if (parent !== parentKey) {
+      QUESTIONS[parentKey].deleteChildQuestion(key);
+    }
+    QUESTIONS[parentKey].addChildQuestion(newQuestion);
+    QUESTIONS[key] = newQuestion;
+    console.log(QUESTIONS);
+  }
+
+  startDecisionTree = function (e) {
+    e.stopPropagation();
+    let answer = firstQuestion.ask();
+    let tempQuestion1 = firstQuestion;
+    while (true) {
+      if (answer === "exit") {
+        break;
+      }
+      answer = Number(answer);
+      tempQuestion1 = tempQuestion1.questions[answer];
+      answer = tempQuestion1.ask();
     }
   }
 }
-const QUESTIONS = {};
-const firstQuestion = new Question(1, "Your age?");
-const secondQuestion = new Question(2, "Your shcool?", "Under 18");
-const thirdQuestion = new Question(3, "Your job?", "over 18");
-const tempQuestion = new Question(4, "Ok", "cambridge")
-firstQuestion.addChildQuestion(secondQuestion);
-firstQuestion.addChildQuestion(thirdQuestion);
-secondQuestion.addChildQuestion(tempQuestion);
-QUESTIONS[firstQuestion.key] = firstQuestion;
-QUESTIONS[secondQuestion.key] = secondQuestion;
-QUESTIONS[thirdQuestion.key] = thirdQuestion;
-QUESTIONS[tempQuestion.key] = tempQuestion;
-
-function addQuestion(e) {
-  e.stopPropagation();
-  const key = Number(document.getElementById("key").value);
-  const question = document.getElementById("question").value;
-  const parent = Number(document.getElementById("parent").value);
-  const answer = document.getElementById("answer").value;
-  const newQuestion = new Question(key, question, answer, parent);
-  QUESTIONS[parent].addChildQuestion(newQuestion);
-  QUESTIONS[key] = newQuestion;
-  console.log(QUESTIONS);
-}
-
-function deleteQuestion(e) {
-  e.stopPropagation();
-  const key = Number(document.getElementById("key").value);
-  QUESTIONS[key].deleteAllChildQuestion();
-  QUESTIONS[QUESTIONS[key].parent].deleteChildQuestion(key);
-  delete QUESTIONS[key];
-  console.log(QUESTIONS);
-}
-
-function editQuestion(e) {
-  e.stopPropagation();
-  const key = Number(document.getElementById("key").value);
-  const question = document.getElementById("question").value;
-  const parent = Number(document.getElementById("parent").value);
-  const answer = document.getElementById("answer").value;
-  const newQuestion = new Question(key, question, answer, parent);
-  const parentKey = QUESTIONS[key].parent;
-  if (parent !== parentKey) {
-    QUESTIONS[parentKey].deleteChildQuestion(key);
-  }
-  QUESTIONS[parentKey].addChildQuestion(newQuestion);
-  QUESTIONS[key] = newQuestion;
-  console.log(QUESTIONS);
-}
-
-function startDecisionTree(e) {
-  e.stopPropagation();
-  let answer = firstQuestion.ask();
-  let tempQuestion1 = firstQuestion;
-  while (true) {
-    if (answer === "exit") {
-      break;
-    }
-    answer = Number(answer);
-    tempQuestion1 = tempQuestion1.questions[answer];
-    answer = tempQuestion1.ask();
-  }
-}
+decisionTree();
